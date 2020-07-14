66af35eab49a9f69dcc0b62d735740e7249cae2c
Jay White <JayWhite2357@users.noreply.github.com>
2020-07-14 19:34:30 -0400

    script update

--- makedoc.sh
+++ makedoc.sh
@@ -1,6 +1,7 @@
-cd ~/maxbetti/SeparateFiles
+cd ~/maxbetti
+cd SeparateFiles
 ../M2expand.py MaxBettiNumbers.m2 > ../MaxBettiNumbers.m2
-cd ~/maxbetti/
+cd ..
 echo "uninstallAllPackages;installPackage\"MaxBettiNumbers\";" > installPack.m2
 M2 --script installPack.m2
 rm installPack.m2
--- makedocfull.sh
+++ makedocfull.sh
@@ -1,6 +1,7 @@
-cd ~/maxbetti/SeparateFiles
+cd ~/maxbetti
+cd SeparateFiles
 ../M2expand.py MaxBettiNumbers.m2 > ../MaxBettiNumbers.m2
-cd ~/maxbetti/
+cd ..
 rm -r ~/.Macaulay2/local/share/doc/Macaulay2/MaxBettiNumbers/
 echo "uninstallAllPackages;installPackage\"MaxBettiNumbers\";check MaxBettiNumbers;" > installPack.m2
 M2 --script installPack.m2
